023173514099af6a28b893112faa436f3f284518
VukasinVuji <vukasinvujic021@gmail.com>
2019-02-13 13:15:57 +0100

    adding comment table

--- resources/views/moviesFolder/show.blade.php
+++ resources/views/moviesFolder/show.blade.php
@@ -8,7 +8,19 @@
 <h3>Genre: {{ $movie->genre }}</h3>
 <h3>Director: {{ $movie->director }}</h3> 
 <p>Year of production: {{ $movie->year }}</p>
-<p>Storyline: {{ $movie->storyline }}</p>    
+<p>Storyline: {{ $movie->storyline }}</p>
+
+
+@foreach($movie->comments as $comment)
+   <div class='p-4 alert-success'>
+       <div class="text-muted">{{$comment->created_at}}</div>
+
+       {{'Comment: '.$comment->content}}
+   </div>
+   <hr>
+   @endforeach
+
+
 @endsection
 
 
