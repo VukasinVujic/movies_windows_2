693a1287091a44bfc01ba06719474cd286f59c63
VukasinVuji <vukasinvujic021@gmail.com>
2019-02-13 12:01:58 +0100

    Nothing special just add a little bit in show.blade

--- resources/views/moviesFolder/show.blade.php
+++ resources/views/moviesFolder/show.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
 
-{{ $movie->title }}    
 
+<h1>{{ $movie->title }}</h1>
+<br>
+<h3>Genre: {{ $movie->genre }}</h3>
+<h3>Director: {{ $movie->director }}</h3> 
+<p>Year of production: {{ $movie->year }}</p>
+<p>Storyline: {{ $movie->storyline }}</p>    
 @endsection
 
 
